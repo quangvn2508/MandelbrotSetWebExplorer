3105ace830b542d1a355820bf6b2dd18a25e3c54
Vinh Quang Nguyen <quangvn2508@gmail.com>
2020-08-02 18:32:24 +0700

    Minor

--- Main.js
+++ Main.js
@@ -34,7 +34,7 @@ function resizeWindow() {
 }
 
 function draw() {
-    var image = currentMBS, x, y;
+    let image = currentMBS, x, y;
     if (imageData === undefined) return;
     x = ((image.MBSData.minR - newMBS.MBSData.minR) / newMBS.MBSData.rangeX * newMBS.resolution.x) | 0;
     y = ((image.MBSData.minI - newMBS.MBSData.minI) / (newMBS.MBSData.rangeX) * newMBS.resolution.x) | 0;
@@ -52,9 +52,8 @@ function upToDate() {
 
 // ***************** WORKER ***********************
 // variable used in workers
-var partialWorkerCount = 0, MBSMatrixPartial, frequency, numPoints, timer = 0, worker = undefined, isWorking = false, y, i, minPixelsPerColorPoint, curPixelsStack, curColorPoint, colorCode;
+var partialWorkerCount = 0, MBSMatrixPartial, frequency, numPoints, timer = 0, worker = undefined, isWorking = false, minPixelsPerColorPoint, curPixelsStack, curColorPoint, colorCode;
 function startPartialWorker() {
-    var y;
     if (!isWorking && !upToDate()) {
         timer = new Date(); // Ellapsed timer
 
@@ -62,7 +61,7 @@ function startPartialWorker() {
         frequency = new Array(newMBS.MBSData.maxIteration).fill(0);
         numPoints = 0;
         
-        for (y = 0; y < newMBS.resolution.y; y+=BLOCK) {
+        for (let y = 0; y < newMBS.resolution.y; y+=BLOCK) {
             startAWorker(y);
             partialWorkerCount++;
         }
@@ -88,7 +87,7 @@ function generateColorCode() {
     
     colorCode = new Array(currentMBS.MBSData.maxIteration).fill(0);
 
-    for (i = 0; i < currentMBS.MBSData.maxIteration; i++) {
+    for (let i = 0; i < currentMBS.MBSData.maxIteration; i++) {
         curPixelsStack += frequency[i];
         // cumulative frequency large enough to move to next color value.
         if (curPixelsStack >= minPixelsPerColorPoint) {
@@ -108,11 +107,11 @@ function generateColorCode() {
 }
 
 function setDataFromWorker(data) {
-    var y;
-    for (y = data.params.minY; y <= data.params.maxY; y++) {
+
+    for (let y = data.params.minY; y <= data.params.maxY; y++) {
         MBSMatrixPartial[y] = data.MBSMatrix[y - data.params.minY];
     }
-    for (i = 0; i < data.params.MBSData.maxIteration; i++) {
+    for (let i = 0; i < data.params.MBSData.maxIteration; i++) {
         frequency[i] += data.FrequencyData.Frequency[i];
     }
     numPoints += data.FrequencyData.Count;
@@ -121,13 +120,12 @@ function setDataFromWorker(data) {
 function makeImage() {
     imageData = context.createImageData(currentMBS.resolution.x, currentMBS.resolution.y);
     
-    for (i = 0; i < imageData.data.length/4; i++) {
+    for (let i = 0; i < imageData.data.length/4; i++) {
         if (MBSMatrixPartial[(i/currentMBS.resolution.x)|0][(i%currentMBS.resolution.x)] === currentMBS.MBSData.maxIteration) {
-            imageData.data[i*4] = 0;
+            imageData.data[i*4+3] = 255;
         } else {
-            imageData.data[i*4] = colorCode[MBSMatrixPartial[(i/currentMBS.resolution.x)|0][(i%currentMBS.resolution.x)]];
+            imageData.data[i*4+3] = 255 - colorCode[MBSMatrixPartial[(i/currentMBS.resolution.x)|0][(i%currentMBS.resolution.x)]];
         }
-        imageData.data[i*4+3] =  255;
     }
 }
 
@@ -150,7 +148,7 @@ function receivePartialMBSData(e) {
 // Mandelbrot Set functionalities
 async function scroll(e) {
     const zoomFactor =  1 + (e.deltaY>0? 1 : -1)*SCROLL_FACTOR;
-    var splitX, splitY;
+    let splitX, splitY;
     splitX = newMBS.MBSData.rangeX * (e.pageX / newMBS.resolution.x) + newMBS.MBSData.minR;
     splitY = newMBS.MBSData.rangeX * (e.pageY / newMBS.resolution.x) + newMBS.MBSData.minI;
 
--- worker.js
+++ worker.js
@@ -1,7 +1,3 @@
-/**
- * {Resolution{x, y}, minR, minI, rangeX, maxIteration, radiusSquare}
- */
-
 var MBSMatrix, frequency, numPoints;
 
 this.onmessage = function(e) {
@@ -15,8 +11,8 @@ this.onmessage = function(e) {
 function getFrequency(params) {
     frequency = new Array(params.MBSData.maxIteration).fill(0);
     numPoints = 0;
-    for (y = params.minY; y <= params.maxY; y++) {
-        for (x = 0; x < params.Resolution.x; x++) {
+    for (let y = params.minY; y <= params.maxY; y++) {
+        for (let x = 0; x < params.Resolution.x; x++) {
             if (MBSMatrix[y-params.minY][x] < params.MBSData.maxIteration) {
                 frequency[MBSMatrix[y-params.minY][x]]++;
                 numPoints++;
@@ -26,7 +22,7 @@ function getFrequency(params) {
 }
 
 function calcMandel(Real, Img, maxIt, r2) {
-    var it = 0, zr = 0, zi = 0, zr2, zi2, nzr, nzi;
+    let it = 0, zr = 0, zi = 0, zr2, zi2, nzr, nzi;
 
     while (it < maxIt) {
         zr2 = zr * zr;
@@ -43,75 +39,16 @@ function calcMandel(Real, Img, maxIt, r2) {
 }
 
 function calMandelbrotSet(params) {
-    var step, x, y, Real, Img;
+    let step, Real, Img;
 
     MBSMatrix = new Array(params.maxY - params.minY + 1).fill(0)
     .map(() => new Array(params.Resolution.x).fill(0));
 
     step = params.MBSData.rangeX / params.Resolution.x;
 
-    for (y = params.minY; y <= params.maxY; y++) {
+    for (let y = params.minY; y <= params.maxY; y++) {
         Img = params.MBSData.minI + step*(0.5 + y);
-        for (x = 0; x < params.Resolution.x; x++) {
-            Real = params.MBSData.minR + step*(0.5 + x);
-            MBSMatrix[y-params.minY][x] = calcMandel(Real, Img, params.MBSData.maxIteration, params.MBSData.radiusSquare);
-        }
-    }
-}/**
- * {Resolution{x, y}, minR, minI, rangeX, maxIteration, radiusSquare}
- */
-
-var MBSMatrix, frequency, numPoints;
-
-this.onmessage = function(e) {
-    if (e.data.params !== undefined) {
-        calMandelbrotSet(e.data.params);
-        getFrequency(e.data.params);
-        this.postMessage({params: e.data.params, MBSMatrix : MBSMatrix, FrequencyData: {Frequency : frequency, Count: numPoints}});
-    }
-}
-
-function getFrequency(params) {
-    frequency = new Array(params.MBSData.maxIteration).fill(0);
-    numPoints = 0;
-    for (y = params.minY; y <= params.maxY; y++) {
-        for (x = 0; x < params.Resolution.x; x++) {
-            if (MBSMatrix[y-params.minY][x] < params.MBSData.maxIteration) {
-                frequency[MBSMatrix[y-params.minY][x]]++;
-                numPoints++;
-            }
-        }
-    }
-}
-
-function calcMandel(Real, Img, maxIt, r2) {
-    var it = 0, zr = 0, zi = 0, zr2, zi2, nzr, nzi;
-
-    while (it < maxIt) {
-        zr2 = zr * zr;
-        zi2 = zi * zi;
-        nzr = zr2 - zi2 + Real;
-        nzi = 2 * zr * zi + Img;
-        zr = nzr;
-        zi = nzi;
-        if ((zr2 + zi2) > r2)
-            break;
-        it++;
-    }
-    return it;
-}
-
-function calMandelbrotSet(params) {
-    var step, x, y, Real, Img;
-
-    MBSMatrix = new Array(params.maxY - params.minY + 1).fill(0)
-    .map(() => new Array(params.Resolution.x).fill(0));
-
-    step = params.MBSData.rangeX / params.Resolution.x;
-
-    for (y = params.minY; y <= params.maxY; y++) {
-        Img = params.MBSData.minI + step*(0.5 + y);
-        for (x = 0; x < params.Resolution.x; x++) {
+        for (let x = 0; x < params.Resolution.x; x++) {
             Real = params.MBSData.minR + step*(0.5 + x);
             MBSMatrix[y-params.minY][x] = calcMandel(Real, Img, params.MBSData.maxIteration, params.MBSData.radiusSquare);
         }
